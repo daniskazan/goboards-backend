15f72753d08f0ae7c6847de1adea4f02ad7ff37f
Danis Sharafiev <d.sharafiev@bars-open.ru>
2024-09-10 23:40:05 +0300

    refactor

---
+++ src/core/auth/repository/db/query_builders/delete_refresh_token.py
@@ -0,0 +1,34 @@
+import uuid
+from typing import Self
+
+from sqlalchemy.sql import Delete, delete
+
+from core.auth.models import SessionORM
+
+
+class DeleteRefreshSessionQueryBuilder:
+    __result_query: Delete = ...
+
+    @classmethod
+    def __select_sessions(cls) -> type[Self]:
+        cls.__result_query = delete(SessionORM)
+        return cls
+
+    @classmethod
+    def __filter_by_value(cls, *, refresh_token: uuid.UUID) -> type[Self]:
+        cls.__result_query = cls.__result_query.where(SessionORM.refresh_token == refresh_token)
+        return cls
+
+    @classmethod
+    def __build(cls) -> Delete:
+        return cls.__result_query
+
+    @classmethod
+    def build(cls, *, refresh_token: uuid.UUID) -> Delete:
+        q = (
+            cls
+            .__select_sessions()
+            .__filter_by_value(refresh_token=refresh_token)
+            .__build()
+        )
+        return q
